b0f71bd974b82ecb63d47c62e7fd65ba91fb0366
Renan <renan.fachin@hotmail.com>
2022-09-12 00:13:23 -0300

    Instalado o typescript como dependencia e feita melhorias no método get

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -8,8 +8,16 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', () => {
-    console.log('Acessou Ads!')
+app.get('/ads', (request, response) => {
+    // request e response são padrões do express
+    // request faz a busca de informações por requisições na rota
+    // response devolve uma resposta
+
+    return response.json([
+        { id: 1, name: 'Anúncio 1' },
+        { id: 2, name: 'Anúncio 2' },
+        { id: 3, name: 'Anúncio 3' },
+    ])
 })
 
 app.listen(3333)
