14cec76daea9ba4d5ba4b96801a4fb9f44be60ea
Renan <renan.fachin@hotmail.com>
2022-09-12 22:49:56 -0300

    Estilização do index.html com tailwind e feito import deste arquivo para os arquivos padrões do tailwind

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     // Definindo onde serão os locais que terão os arquivos html que vão conter classes do tailwind
-    './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
     extend: {},
