77922493030499aa1a17ea9688ff348c57da8786
Renan <renan.fachin@hotmail.com>
2022-09-13 11:39:50 -0300

    mobile: Feita a implementação da imagem de logo na screen Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+// Importando o logo
+import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+
+      <Image 
+        source={logoImg}
+        // style é para definir de onde ele vai trazer as infos de estilização
+        style={styles.logo}
+      />
 
     </View>
   );
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,5 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  // Estilização da imagem de logo
+  logo:{
+    width: 214,
+    height: 120,
+    marginTop: 74,
+    marginBottom: 48,
+  }
 });
