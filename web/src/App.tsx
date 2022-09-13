dd86960007cfc0f9042bf9ef1d27b0dac69a0ae9
Renan <renan.fachin@hotmail.com>
2022-09-12 23:21:31 -0300

    Feito as primeira estilizações de texto e adicionada um novo theme de bg para o tailwind config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,7 +8,14 @@ function App() {
   return(
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       {/* Colocando a logo */}
-      <img src={logoImg} />
+      <img 
+      src={logoImg} 
+      />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo</span> está aqui
+      </h1>
+
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -9,7 +9,8 @@ module.exports = {
     extend: {
       backgroundImage:{
         // Fazendo o import do bg-galaxy.png e chamando ele de galaxy
-        galaxy: "url('/bg-galaxy.png')"
+        galaxy: "url('/bg-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
       },
     },
   },
