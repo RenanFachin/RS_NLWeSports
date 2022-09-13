6de7f3192d07bbe90204823028956550fb82a321
Renan <renan.fachin@hotmail.com>
2022-09-13 11:31:20 -0300

    Importa a fonte INTER a ser utilizada no projeto, implementa um check para saber se as fontes já foram carregadas antes de rendizar algo na página, cria a estrutura da screen HOME e cria a estrutura do componente LOADING

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,34 @@
-// Importação 
+// Importações de "componentes nativos" do react-native
 import { StatusBar } from 'react-native'
+
+// Importações personalizadas (fonts)
+import { 
+  useFonts, 
+  Inter_400Regular,
+  Inter_600SemiBold, 
+  Inter_700Bold,
+  Inter_900Black
+ } from '@expo-google-fonts/inter'
 
 // Importação do componente Background
 import { Background } from './src/components/Background';
 
+// Importação das Screens
+import { Home } from './src/screens/Home'
+
+
 export default function App() {
+  // useFonts é o responsável pelo carregamento das fontes
+  // fontsLoaded é uma constante criada para GARANTIR que as fontes tenham já sido inicializadas
+  // fontsLoaded é do tipo booleano
+  const [fontsLoaded] = useFonts({  
+    Inter_400Regular,
+    Inter_600SemiBold, 
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+
   return (
     <Background>
       <StatusBar 
@@ -13,6 +37,11 @@ export default function App() {
         // fazendo os dados da statusbar sobrepor
         translucent
       />
+
+      {/* Verificando se as fontes já foram inicializadas */}
+      {/* Se a fonte já foi carregada = exibir a página HOME */}
+      {/* Caso contrário, mostrar um componente de LOADING */}
+      { fontsLoaded ? <Home /> : }
       
     </Background>
   );
---
+++ mobile/src/components/Loading/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    // Flex: 1 é para o componente utilizar sempre o máximo do tamanho disponível
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
