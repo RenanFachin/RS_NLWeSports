8ac4635ada8d5e1cae687d324acdc967eb2f9981
Renan <renan.fachin@hotmail.com>
2022-09-13 11:49:38 -0300

    Adiciona o componente Heading na screen Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -4,6 +4,10 @@ import { View, Image } from 'react-native';
 // Importando o logo
 import logoImg from '../../assets/logo-nlw-esports.png'
 
+// Importação de componentes
+import { Heading } from '../../components/Heading';
+
+// Importação dos styles desta página
 import { styles } from './styles';
 
 export function Home() {
@@ -15,6 +19,12 @@ export function Home() {
         // style é para definir de onde ele vai trazer as infos de estilização
         style={styles.logo}
       />
+
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar"
+      />
+
 
     </View>
   );
