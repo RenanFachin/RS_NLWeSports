24ac5f521a4704c9865d57a169982e2949fc1b96
Renan <renan.fachin@hotmail.com>
2022-09-14 01:50:16 -0300

    Inicia a integração do banco de dados com os métodos http

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,13 @@
 // Importando com ECMAScript Module
 import express from 'express'
 
+// Importando o prismaClient
+import { PrismaClient } from '@prisma/client'
+
 const app = express()
+const prisma = new PrismaClient({
+    log: ['query']
+}) // Faz a conexão com o banco de forma automática
 
 // HTTP methods / API RESTful / HTTP CODE
 // GET, POST, PUT, PATCH, DELETE
@@ -16,8 +22,21 @@ const app = express()
 // É possível se ter mesmo nome de rotas mas com métodos http diferentes
 
 // listando os games
-app.get('/games', (request, response) => {
-    return response.json([]) // retornando vazio apenas para não dar erro
+app.get('/games', async (request, response) => {
+    // O método findMany "demora" para retornar com os dados do banco de dados
+    // Por este motivo, é necessário o async/await
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+
+
+    return response.json(games) // retornando vazio apenas para não dar erro
 })
 
 // publicando(criando) um novo anúncio
@@ -27,20 +46,43 @@ app.post('/ads', (request, response) => {
 
 // Listagem de anúncios POR game
 // utilizando route params para acessar o anuncio POR game
-app.get('/games/:id/ads', (request, response) => {
-    // const gameId = request.params.id
+app.get('/games/:id/ads', async(request, response) => {
+    const gameId = request.params.id
+
+    const ads = await prisma.ad.findMany({
+        // Dizendo quais são os campos que vão aparecer (retirado o campo discord)
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        // Fazendo a busca
+        where:{
+            gameId: gameId
+        },
+        // Listando os dados no db apartir dos anúncios mais recentes
+        orderBy: {
+            createAt: 'desc',
+        }
+    })
 
     // request e response são padrões do express
     // request faz a busca de informações por requisições na rota
     // response devolve uma resposta
 
-    return response.json([
-        { id: 1, name: 'Anúncio 1' },
-        { id: 2, name: 'Anúncio 2' },
-        { id: 3, name: 'Anúncio 3' },
-        { id: 4, name: 'Anúncio 4' },
-        { id: 5, name: 'Anúncio 5' },
-    ])
+    // Percorrendo os ads e para cada dado dentro de ads = terá uma formatação
+    return response.json(ads.map(ad => {
+        return{
+            // spread operator em todo o objeto e substituindo apenas o weekdays
+            ...ad,
+            // Fazendo a separação do weekdays onde tem a vírgula (vai retornar um array e cada posição do array diz qual dia da semana que é)
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 })
 
 
