739120b1b2446a1f8ecfb4bbc9e20f5fdd73a052
Renan <renan.fachin@hotmail.com>
2022-09-15 14:17:05 -0300

    mobile: Inicia a página Game. Importa o useRoutes, inicia ele e também atribui os parâmetros obtidos em uma constante

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,5 +1,6 @@
 // Importando bibliotecas e dependências
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute } from '@react-navigation/native' // serve para resgatar as infos que vem da rota
 
 // Importando componente
 import { Background } from '../../components/Background';
@@ -7,6 +8,15 @@ import { Background } from '../../components/Background';
 import { styles } from './styles';
 
 export function Game() {
+  
+  // Iniciando o useRoute
+  const route = useRoute();
+
+  // Pegando os parâmetros que são obtidos e colocando eles na constante game
+  // É necessário criar uma tipagem para que ele possa saber os parâmetros que ele recebe
+  const game = route.params;
+  console.log(game)
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
