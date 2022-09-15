32e561bd407f38b9d3d7260d55fc56a521d5dcf1
Renan <renan.fachin@hotmail.com>
2022-09-15 01:36:53 -0300

    web: Cria a estrutura do formulário (sem estilização)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,6 +14,9 @@ import logoImg from './assets/logo-nlw-esports.svg'
 import { GameBanner } from './components/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner'
 
+// Importando icons do phosphor-react
+import { GameController } from 'phosphor-react'
+
 //  Interface de como vem os dados da API
 interface Game {
   id: string;
@@ -72,10 +75,60 @@ function App() {
           <Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
 
           <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
-            <Dialog.Title>Publique um anúncio</Dialog.Title>
+            <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
 
             <Dialog.Content>
-              teste
+              <form>
+                <div>
+                   {/* htmlFor do label precisa ser igual ao id do input */}
+                  <label htmlFor="game">Qual o game?</label>
+                  <input id="game" placeholder='Selecione o game que deseja jogar'/>
+                </div>
+
+                <div>
+                  <label htmlFor="name">Seu nome (ou nickname)</label>
+                  <input id="name" placeholder='Como te chamam dentro do game?'/>
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                    <input id="yearsPlaying" type="number" placeholder='Tudo bem ser ZERO'/>
+                  </div>
+
+                  <div>
+                    <label htmlFor="discord">Qual seu discord?</label>
+                    <input id='discord' placeholder='Usuario#0000' />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="weekDays">Quando costuma jogar?</label>
+                    
+                  </div>
+                  <div>
+                    <label htmlFor="hourStart">Qual horário do dia?</label>
+                    <div>
+                      <input id="hourStart" type="time" placeholder='De'/>
+                      <input id="hourEnd" type="time" placeholder='Até'/>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <input type="checkbox" /> Costumo me conectar ao chat de voz
+                </div>
+
+                <footer>
+                  <button>Cancelar</button>
+                  {/* O botão com type submit é o que faz o envio do formulário */}
+                  <button type='submit'> 
+                  <GameController />
+                  Encontrar duo
+                  </button>
+                </footer>
+              </form>
             </Dialog.Content>
 
           </Dialog.Content>
