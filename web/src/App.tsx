6e722ec46d5e451593ea901706d9f07e58c50f00
Renan <renan.fachin@hotmail.com>
2022-09-15 01:26:54 -0300

    web: Feita a estilização do content do modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -71,7 +71,7 @@ function App() {
         <Dialog.Portal>
           <Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
 
-          <Dialog.Content>
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
             <Dialog.Title>Publique um anúncio</Dialog.Title>
 
             <Dialog.Content>
