75d654034ec09b719eff57d3207661553fd8a204
Renan <renan.fachin@hotmail.com>
2022-09-16 01:06:19 -0300

    web: Capturando valores de input nativos

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -10,7 +10,7 @@ import * as ToggleGroup from '@radix-ui/react-toggle-group'
 import { Input } from './Form/Input'
 
 // Importações de hooks do react
-import { useState, useEffect } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 
 //  Interface de como vem os dados da API
 interface Game {
@@ -35,22 +35,31 @@ export function CreateAdModal(){
       })
     },[])
 
+    function handleCreateAd(event: FormEvent){
+        event.preventDefault();
+
+        const formData = new FormData(event.target as HTMLFormElement)
+        const data = Object.fromEntries(formData)
+
+        console.log(data)
+    }
 
 
     return(
     <Dialog.Portal>
-    <Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
+        <Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
 
-    <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
-      <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
+            <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+                <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
 
 
-        <form className='mt-8 flex flex-col gap-4'>
+        <form onSubmit={handleCreateAd} className='mt-8 flex flex-col gap-4'>
 
           <div className='flex flex-col gap-2'>
              {/* htmlFor do label precisa ser igual ao id do input */}
             <label htmlFor="game" className='font-semibold'>Qual o game?</label>
-            <select 
+            <select
+            name='game' 
             id='game' 
             className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
             defaultValue=""
@@ -67,7 +76,7 @@ export function CreateAdModal(){
 
           <div className='flex flex-col gap-2'>
             <label htmlFor="name">Seu nome (ou nickname)</label>
-            <Input id="name" placeholder='Como te chamam dentro do game?'/>
+            <Input name="name" id="name" placeholder='Como te chamam dentro do game?'/>
           </div>
 
           {/* className='grid grid-cols-2 gap-6' = deixando um elemento ao lado do outro 
@@ -76,12 +85,12 @@ export function CreateAdModal(){
           <div className='grid grid-cols-2 gap-6'>
             <div className='flex flex-col gap-2'>
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-              <Input id="yearsPlaying" type="number" placeholder='Tudo bem ser ZERO'/>
+              <Input name="yearsPlaying" id="yearsPlaying" type="number" placeholder='Tudo bem ser ZERO'/>
             </div>
 
             <div className='flex flex-col gap-2'>
               <label htmlFor="discord">Qual seu discord?</label>
-              <Input id='discord' placeholder='Usuario#0000' />
+              <Input name='discord' id='discord' placeholder='Usuario#0000' />
             </div>
           </div>
 
@@ -160,8 +169,8 @@ export function CreateAdModal(){
             <div className='flex flex-col gap-2 flex-1'>
               <label htmlFor="hourStart">Qual horário do dia?</label>
               <div className='grid grid-cols-2 gap-2'>
-                <Input id="hourStart" type="time" placeholder='De'/>
-                <Input id="hourEnd" type="time" placeholder='Até'/>
+                <Input name="hourStart" id="hourStart" type="time" placeholder='De'/>
+                <Input name="hourEnd" id="hourEnd" type="time" placeholder='Até'/>
               </div>
             </div>
 
