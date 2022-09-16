7a5c7a093d0d4abfe86816118ba3aa21e03500a4
Renan <renan.fachin@hotmail.com>
2022-09-16 18:00:58 -0300

    mobile: Finalizada a estrutura e estilização do componente(modal) DuoMatch

--- mobile/src/components/DuoMatch/index.tsx
+++ mobile/src/components/DuoMatch/index.tsx
@@ -1,5 +1,6 @@
 // Importando componentes
 import { View, Modal, ModalProps, Text, TouchableOpacity } from 'react-native';
+import { Heading } from '../Heading';
 
 // Importando estilos
 import { styles } from './styles';
@@ -7,6 +8,7 @@ import { THEME } from '../../theme';
 
 // importando ícones
 import { MaterialIcons } from '@expo/vector-icons'
+import { CheckCircle } from 'phosphor-react-native'
 
 interface Props extends ModalProps {
   discord: string;
@@ -25,6 +27,29 @@ export function DuoMatch({ discord, onClose, ...rest }: Props) {
           <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
             <MaterialIcons name='close' size={20} color={THEME.COLORS.CAPTION_500}/>
           </TouchableOpacity>
+
+
+          <CheckCircle
+            size={64}
+            color={THEME.COLORS.SUCCESS}
+            // Manipulando a espessura dos ícones com a propriedade weight
+            weight='bold'
+          />
+
+          <Heading 
+            title="Let'sPlay"
+            subtitle='Agora é só começar a jogar!'
+            // Manipulando o heading para que ele fique em uma posição diferente
+            style={{ alignItems: 'center', marginTop: 24 }}
+          />
+
+          {/* Fazendo este texto ser clicável com a ajuda do touchableOpacity */}
+          <TouchableOpacity style={styles.discordButton}>
+            <Text style={styles.label}>
+              Adicione no Discord
+            </Text>
+          </TouchableOpacity>
+
 
           <Text style={styles.discord}>
             {discord}
