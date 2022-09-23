ce6f09a2238c983fbb1e01cc273643394990f6ea
Renan <renan.fachin@hotmail.com>
2022-09-23 15:42:13 -0300

    :sparkles: feat: Cria página de autenticação com o discord

--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -2,6 +2,7 @@ import { Image, TouchableOpacity, Text } from 'react-native';
 
 // Importando bibliotecas e dependências
 import { SafeAreaView } from 'react-native-safe-area-context'
+import * as AuthSession from 'expo-auth-session'
 
 // Importando o logo
 import logoImg from '../../assets/logo-nlw-esports.png'
@@ -20,6 +21,19 @@ export function SignIn() {
 
   async function handleDiscordSignIn(){
 
+      const response = await AuthSession.startAsync({
+        authUrl: "https://discord.com/api/oauth2/authorize?client_id=1022931634296733856&redirect_uri=https%3A%2F%2Fauth.expo.io%2Ffachin182%2Fmobile&response_type=token&scope=identify"
+      });
+
+      fetch('https:/discord.com/api/users/@me', {
+        headers: {
+          'authorization': `Bearer ${response.params.acess.token}`
+        }
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+       
+      console.log(response);
   }
 
   return (
